e62edd52fa3a871e0e447d36e97470b79fcea88d
Ryanhilde <48308793+Ryanhilde@users.noreply.github.com>
2019-10-05 10:29:12 -0700

    Add files via upload

---
+++ RyanHildebrantSoftware2/src/ryanhildebrantsoftware2/RyanHildebrantSoftware2.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ryanhildebrantsoftware2;
+
+import Database.DBConnection;
+import Model.Appointment;
+import Model.Customer;
+import Model.DataProvider;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ryanh
+ */
+public class RyanHildebrantSoftware2 extends Application {
+    
+    
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/LoginScreen.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+        
+        
+        launch(args);
+         
+           
+    }
+    
+}
